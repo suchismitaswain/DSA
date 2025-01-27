3b168d2b1315e6a8a31afd52be04c318cc1e1e7e
Suchismita Swain <123295737+suchismitaswain@users.noreply.github.com>
2025-01-27 23:14:01 +0530

    deleted

--- Python/Leetcode1930.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def countPalindromicSubsequence(self, s: str) -> int:
-        res=0
-        for c in set(s):
-            i,j=s.find(c),s.rfind(c)
-            if j>i+1:
-                res+= len(set(s[i+1:j]))
-        return res
