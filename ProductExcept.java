f9bbc787f24fc8857d1b70eaf42448380c369ba0
Suchismita Swain <123295737+suchismitaswain@users.noreply.github.com>
2025-02-18 23:37:48 +0530

    Update ProductExcept.java

--- ProductExcept.java
+++ ProductExcept.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
         Arrays.fill(ans, 1);
+        
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if (i != j) {
