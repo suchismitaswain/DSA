035564d827037942f0a370a560a807997839e4d9
Suchismita Swain <123295737+suchismitaswain@users.noreply.github.com>
2025-02-18 23:33:28 +0530

    Update sortIntByPower.py

--- Python/sortIntByPower.py
+++ Python/sortIntByPower.py
@@ -3,8 +3,10 @@ class Solution:
         C=0
         F=[]
         E=[i for i in range(lo,hi+1)]
+        
         for i in range(lo,hi+1):
             C=0
+            
             while(i>1):
                 if i%2==0:
                     i//=2
@@ -12,6 +14,7 @@ class Solution:
                     i=3*i+1
                 C+=1
             F.append(C)
+            
         return [x for _, x in sorted(zip(F,E))][k-1]
 
         
