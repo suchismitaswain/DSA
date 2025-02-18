24b264cb82066148936aceb9c058fd247fce94f7
Suchismita Swain <123295737+suchismitaswain@users.noreply.github.com>
2025-02-18 23:35:23 +0530

    Update NestedSwitch.java

--- NestedSwitch.java
+++ NestedSwitch.java
@@ -26,8 +26,7 @@ public class NestedSwitch {
                 }
                 
             default:
-                System.out.println("Enter correcr empID");
-                
+                System.out.println("Enter correcr empID");      
         }
 
     }
