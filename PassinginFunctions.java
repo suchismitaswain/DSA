2362ae37864c5acafdf1a7dc6e6aa88b77cc8387
Suchismita Swain <123295737+suchismitaswain@users.noreply.github.com>
2025-02-18 23:36:34 +0530

    Update PassinginFunctions.java

--- PassinginFunctions.java
+++ PassinginFunctions.java
@@ -5,6 +5,7 @@ public class PassinginFunctions {
         change (nums);
         System.out.println(Arrays.toString(nums));
     }
+    
     static void change(int [] arr){
         arr[0] =99;
     }
