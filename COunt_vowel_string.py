3b78dd5604cc0becdb07f19ffe81674bcc291927
Suchismita Swain <123295737+suchismitaswain@users.noreply.github.com>
2025-02-24 00:17:17 +0530

    Update COunt_vowel_string.py

--- COunt_vowel_string.py
+++ COunt_vowel_string.py
@@ -5,7 +5,6 @@ class Solution(object):
         count = 0
         def isVowel(c):
             return c in 'aeiou'
-
         for word in words:
             if isVowel(word[0]) and isVowel(word[-1]):
                 count += 1
@@ -17,6 +16,5 @@ class Solution(object):
                 res.append(prefix[r])
             else:
                 res.append(prefix[r] - prefix[l - 1])
-
         return res
         
